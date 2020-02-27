eeba5751280356adb128f7e6a4b4a34485f09361
chiuchiuchiu <chiuhey@126.com>
2020-02-28 00:22:49 +0800

    前端页面

--- apps/business/views/shop/_form.php
+++ apps/business/views/shop/_form.php
@@ -14,23 +14,23 @@ $form = ActiveForm::begin(['layout' => 'horizontal']);
 
 <?php
 
-echo $form->field($model,'name')->textInput();
-echo $form->field($model,'free_shipping')->radioList(
+echo $form->field($model, 'name')->textInput();
+echo $form->field($model, 'free_shipping')->radioList(
     FreightTemplate::freeMap(),
-    ['item' => function($index, $label, $name, $checked, $value){
-            $return = '<div class="radio i-checks checkbox-inline">
+    ['item' => function ($index, $label, $name, $checked, $value) {
+        $return = '<div class="radio i-checks checkbox-inline">
                         <label class="radioShipping">
                             <div class="iradio_square-green" style="position: relative;">
                                 <input type="radio" name="FreightTemplate[free_shipping]" value="';
-            $return .= $value;
-            $return .= '" style="position: absolute; opacity: 0;">
+        $return .= $value;
+        $return .= '" style="position: absolute; opacity: 0;">
                                 <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins>
                             </div>&nbsp;';
-            $return .= $label;
-            $return .= '</label></div>';
+        $return .= $label;
+        $return .= '</label></div>';
 
-            return $return;
-        }
+        return $return;
+    }
     ]
 );
 ?>
@@ -55,9 +55,9 @@ echo $form->field($model,'free_shipping')->radioList(
     </div>
 
 <?php
-echo $form->field($model,'transport')->radioList(
+echo $form->field($model, 'transport')->radioList(
     FreightTemplate::transportMap(),
-    ['item' => function($index, $label, $name, $checked, $value){
+    ['item' => function ($index, $label, $name, $checked, $value) {
         $return = '<div class="radio i-checks checkbox-inline">
                         <label class="radioTransport">
                             <div class="iradio_square-green" style="position: relative;">
@@ -74,87 +74,90 @@ echo $form->field($model,'transport')->radioList(
     ]
 );
 ?>
-    <div class="form-group field-shop-name setTransport">
-        <label class="control-label col-sm-3" for="shop-name"></label>
-        <div class="col-sm-10">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th class="col-sm-4">送运到</th>
-                    <th>首件</th>
-                    <th>首费</th>
-                    <th>续件</th>
-                    <th>续费</th>
-                    <th>操作</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr class="success">
-                    <td><button data-toggle="modal" data-target="#create-modal">选择地区</button></td>
-                    <td><input name=""/></td>
-                    <td><input></td>
-                    <td>23/11/2013</td>
-                    <td>待发货</td>
-                </tr>
-                </tbody>
-            </table>
+    <div class="form-group field-shop-name setTransport" style="display: none;">
+        <div class="col-sm-12">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th class="text-center">送运到</th>
+                        <th class="text-center">首件</th>
+                        <th class="text-center">首费</th>
+                        <th class="text-center">续件</th>
+                        <th class="text-center">续费</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="success">
+                        <td>
+                            <button class="form-control" data-toggle="modal" data-target="#create-modal">设置地区</button>
+                        </td>
+                        <td class="col-lg-1"><input class="form-control" name=""/></td>
+                        <td class="col-lg-2"><input class="form-control" name=""/></td>
+                        <td class="col-lg-1"><input class="form-control" name=""/></td>
+                        <td class="col-lg-2"><input class="form-control" name=""/></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-1"></div>
+
         </div>
     </div>
-<!--<div class="form-group field-freighttemplate-transport_type required">-->
-<!--    <label class="control-label col-sm-3" for="freighttemplate-transport_type">配送类型</label>-->
-<!--    <div class="col-sm-6">-->
-<!--        <input type="hidden" name="FreightTemplate[transport_type]" value=""><div id="freighttemplate-transport_type" class="isTransport"><div class="radio i-checks checkbox-inline"><label><div class="iradio_square-green" style="position: relative;"><input type="radio" name="FreightTemplate[transport_type]" value="1" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 全国</label></div>-->
-<!--            <div class="radio i-checks checkbox-inline"><label><div class="iradio_square-green" style="position: relative;"><input type="radio" name="FreightTemplate[transport_type]" value="2" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 部分地区</label></div></div>-->
-<!--        <div class="help-block help-block-error "></div>-->
-<!--    </div>-->
-<!---->
-<!--</div>-->
+    <!--<div class="form-group field-freighttemplate-transport_type required">-->
+    <!--    <label class="control-label col-sm-3" for="freighttemplate-transport_type">配送类型</label>-->
+    <!--    <div class="col-sm-6">-->
+    <!--        <input type="hidden" name="FreightTemplate[transport_type]" value=""><div id="freighttemplate-transport_type" class="isTransport"><div class="radio i-checks checkbox-inline"><label><div class="iradio_square-green" style="position: relative;"><input type="radio" name="FreightTemplate[transport_type]" value="1" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 全国</label></div>-->
+    <!--            <div class="radio i-checks checkbox-inline"><label><div class="iradio_square-green" style="position: relative;"><input type="radio" name="FreightTemplate[transport_type]" value="2" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 部分地区</label></div></div>-->
+    <!--        <div class="help-block help-block-error "></div>-->
+    <!--    </div>-->
+    <!---->
+    <!--</div>-->
 
 
-<div class="row">
-    <div class="form-group">
-        <div class="text-center">
-            <?= \yii\bootstrap\Html::submitButton('提交', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-success']) ?>
+    <div class="row">
+        <div class="form-group">
+            <div class="text-center">
+                <?= \yii\bootstrap\Html::submitButton('提交', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-success']) ?>
+            </div>
         </div>
     </div>
-</div>
 <?php
 ActiveForm::end();
 \components\inTemplate\widgets\IBox::end();
 ?>
 
-<?php \common\widgets\JavascriptBlock::begin();?>
+<?php \common\widgets\JavascriptBlock::begin(); ?>
 
     <script type="text/javascript">
 
         $(function () {
-            $('.radioShipping').on('click', function (){
+            $('.radioShipping').on('click', function () {
                 var values = $(this).find('input').val();
 
-                if(values == <?= FreightTemplate::FREE_SHIPPING_2?>){
+                if (values == <?= FreightTemplate::FREE_SHIPPING_2?>) {
                     $('.setShipping').show();
-                }else{
+                } else {
                     $('.setShipping').hide();
                     $('.setShipping').find('input').val('');
                 }
             });
 
-            $('.radioTransport').on('click', function (){
+            $('.radioTransport').on('click', function () {
 
                 var values = $(this).find('input').val();
 
-                if(values == <?= FreightTemplate::TRANSPORT_ONLY?>){
+                if (values == <?= FreightTemplate::TRANSPORT_ONLY?>) {
                     $(".setTransport").show();
-                }else{
-
+                } else {
+                    $('.setTransport').hide();
+                    $('.setTransport').find('input').val('');
                 }
-                alert(values);
-
-
             })
         });
 
 
     </script>
 
-<?php \common\widgets\JavascriptBlock::end();?>
+<?php \common\widgets\JavascriptBlock::end(); ?>
